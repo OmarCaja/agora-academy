860632d3fe0ba6bee841c548d5d93375a4235e25
Omar Caja <omar.caja@hotmail.com>
2025-11-26 15:46:12 +0100

    [OC] - Improving lazy loadig when scrolling

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ levelTags.forEach((tag) => {
 const initScrollAnimations = () => {
   const observerOptions = {
     threshold: 0.1,
-    rootMargin: "0px 0px -100px 0px",
+    rootMargin: "0px 0px -20px 0px",
   };
 
   const observer = new IntersectionObserver((entries) => {
