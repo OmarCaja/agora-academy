116ef2df655e36b5d5db9c39374b4a55280eb1ea
Omar Caja <omar.caja@hotmail.com>
2025-12-30 13:46:02 +0100

    [OC] - Adding SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://agoraacademy.es',
+    integrations: [sitemap()],
 })
